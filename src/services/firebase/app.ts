f76394ae709741296e8fce64ffd01d8978c26568
Pablo Gonzalez Moncalvillo <pablo@theticketmerchant.com.au>
2025-12-03 23:26:50 +0100

    chore: remover console.log de producción

--- src/services/firebase/app.ts
+++ src/services/firebase/app.ts
@@ -3,7 +3,6 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { FIREBASE_CONFIG } from "../../config/env";
 
-console.log("Firebase config:", FIREBASE_CONFIG);
 const app = getApps().length ? getApp() : initializeApp(FIREBASE_CONFIG);
 
 export const db = getFirestore(app);
